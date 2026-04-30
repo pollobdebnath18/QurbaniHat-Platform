377a02a75f55114334de5f67494adfae4bae5c30
pollob debnath <pullobdebnath@gmail.com>
2026-05-01 01:14:03 +0600

    data.json

--- src/components/homepage/Featured.jsx
+++ src/components/homepage/Featured.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-const animalsData = async () => {
-  const res = await fetch("https://qurbani-hat-platform.vercel.app/data.json");
-  const data = await res.json();
-  console.log(data, "animals");
-  return data;
-};
+// const animalsData = async () => {
+//   const res = await fetch("https://qurbani-hat-platform.vercel.app/data.json");
+//   const data = await res.json();
+//   console.log(data, "animals");
+//   return data;
+// };
 
 const Featured = async () => {
-  const animals = await animalsData();
+//   const animals = await animalsData();
   return (
     <div className="max-w-[1100px] mx-auto">
       this is featured section
-      <h2>Total animals : {animals?.length}</h2>
+      <h2>Total animals : </h2>
     </div>
   );
 };
